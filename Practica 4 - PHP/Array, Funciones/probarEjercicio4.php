6fc4cbb63d4c076da44a438876c6353af537e696
Francisco Mendiburu <37491898+MendiburuFrancisco@users.noreply.github.com>
2022-05-16 17:10:52 -0300

    probando ejercicio 4

--- Practica 4 - PHP/Array, Funciones/probarEjercicio4.php
+++ Practica 4 - PHP/Array, Funciones/probarEjercicio4.php
@@ -1,6 +1,5 @@
 <?php
-
-include 'ejercicio4.php';
-echo " El $flor $color"; 
-
+    include 'ejercicio4.php';
+    echo  comprobar_nombre_usuario('Francisco');
+    echo  comprobar_nombre_usuario('Dr.Strange2020220');
 ?>
